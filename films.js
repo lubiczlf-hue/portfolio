8dea124b245ec95fba6f69d30e1e6f4d3ae700f5
lubiczlf-hue <lubiczlf@gmail.com>
2026-07-12 03:27:17 +0200

    Create films.js

---
+++ films.js
@@ -0,0 +1,51 @@
+// DODAWANIE FILMU:
+// Skopiuj jeden wpis { ... }, wklej go na początku listy i podmień dane.
+// url: link do filmu na Vimeo albo YouTube.
+// image: link do miniatury lub kadru z filmu.
+
+const films = [
+  {
+    title: "Szept",
+    type: "Film fabularny",
+    year: "2025",
+    url: "https://vimeo.com/",
+    image: "https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&w=1600&q=85"
+  },
+  {
+    title: "Piony",
+    type: "Fashion film",
+    year: "2024",
+    url: "https://vimeo.com/",
+    image: "https://images.unsplash.com/photo-1483985988355-763728e1935b?auto=format&fit=crop&w=1600&q=85",
+    section: "piony"
+  },
+  {
+    title: "Po drugiej stronie",
+    type: "Short film",
+    year: "2024",
+    url: "https://youtube.com/",
+    image: "https://images.unsplash.com/photo-1519608487953-e999c86e7455?auto=format&fit=crop&w=1600&q=85",
+    section: "short"
+  },
+  {
+    title: "Nocny kurs",
+    type: "Commercial",
+    year: "2023",
+    url: "https://vimeo.com/",
+    image: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?auto=format&fit=crop&w=1600&q=85"
+  },
+  {
+    title: "Bez słów",
+    type: "Music video",
+    year: "2023",
+    url: "https://youtube.com/",
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?auto=format&fit=crop&w=1600&q=85"
+  },
+  {
+    title: "Światło dzienne",
+    type: "Documentary",
+    year: "2022",
+    url: "https://vimeo.com/",
+    image: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1600&q=85"
+  }
+];
